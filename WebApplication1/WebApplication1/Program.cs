64c0af66b9902e7bc06d29439d9d7c95fe3a5417
Borissov M.R. <borissov.mikhail2374@mail.ru>
2026-02-01 03:22:34 +0300

    Add logging middleware

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpLogging(
+        opts => opts.LoggingFields = HttpLoggingFields.RequestProperties
+    );
+builder.Logging.AddFilter(
+        "Microsoft.AspNetCore.HttpLogging", LogLevel.Information
+    );
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/person", () => new Person("Misha", "Borissov"));
 
 app.Run();
+
+public record Person(string Firstname, string Lastname);
