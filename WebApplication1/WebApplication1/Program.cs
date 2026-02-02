d175b8d1299f215a8af07320acb9d7ac249165ca
Borissov M.R. <borissov.mikhail2374@mail.ru>
2026-02-02 05:00:52 +0300

    ...

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -4,10 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.UseWelcomePage();
+app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.MapGet("/", () => "Helllo world");
+app.MapGet("/", () => "Hello world");
 
 app.Run();
 
